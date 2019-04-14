761c7b041e0c42cd03f604beaa095f6d366b7b56
Marx.W <boyuan@vt.edu>
2019-04-14 13:28:31 -0400

    person updated

--- src/prj5/Person.java
+++ src/prj5/Person.java
@@ -1,5 +1,53 @@
 package prj5;
 
+/**
+ * This is the person class
+ * store survey data of one person
+ */
 public class Person {
+
+    private SongList songList;
+    private Hobby hobby;
+    private Major major;
+    private State state;
+    private int index;
+
+    public Person(SongList thisSongList, 
+        Hobby thisHobby, State thisState, Major thisMajor, int thisIndex)
+    {
+        songList = thisSongList;
+        hobby = thisHobby;
+        major = thisMajor;
+        index = thisIndex;
+    }
+
+    /**
+     * get the hobby data of the person
+     * @return the hobby data
+     */
+    public Hobby getHobby()
+    {
+        return hobby;
+    }
+
+    /**
+     * get the major data of the person
+     * @return the major data
+     */
+    public Major getMajor()
+    {
+        return major;
+    }
+
+    /**
+     * get the state data of the person
+     * @return the state data
+     */
+    public State getState()
+    {
+        return state;
+    }
+
+
 
 }
