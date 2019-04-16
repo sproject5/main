c93d7138f091c2214e42c44c3a0aa44431cd31a4
ConnerMangin <cmangin@vt.edu>
2019-04-16 19:06:15 -0400

    Hobby javadoc

--- src/prj5/Hobby.java
+++ src/prj5/Hobby.java
@@ -1,13 +1,14 @@
 package prj5;
 
 /**
- *  These are the hobbies
+ * These are the hobbies.
+ *
+ * @author Conner Mangin (cmangin)
+ * @version 2019.04.14
  */
 public enum Hobby 
 {
-    READ, ART, SPORTS, MUSIC;
-
-    
+    READ, ART, SPORTS, MUSIC;    
 }
 
 
