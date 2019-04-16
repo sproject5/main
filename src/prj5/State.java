b5ea17a7851b00af9fe6c052ca41962d2727475e
ConnerMangin <cmangin@vt.edu>
2019-04-16 19:10:36 -0400

    javadoc

--- src/prj5/State.java
+++ src/prj5/State.java
@@ -1,8 +1,18 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- cmangin
+
 package prj5;
 
 /**
-* This is the state enum
-*/
+ * These are the states.
+ *
+ * @author Conner Mangin (cmangin)
+ * @version 2019.04.14
+ */
 public enum State 
 {
     NORTHEAST_US, SOUTHEAST_US, THE_REST_OF_US, OUTSIDE_THE_US;
