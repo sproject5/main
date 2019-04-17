56a6f6ba1b67bc2835c104a566c252bc32f64112
ConnerMangin <cmangin@vt.edu>
2019-04-17 18:00:39 -0400

    dataOf changes

--- src/prj5/SurveySorter.java
+++ src/prj5/SurveySorter.java
@@ -10,51 +10,32 @@ package prj5;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
 
+import javax.print.attribute.standard.OutputDeviceAssigned;
 
-/**
- * 
- * @author cmangin
- *  he SurveySorter class will serve to sort data into a songList
- *@version 2019.4
- */
 public class SurveySorter {
 
     private SongList<Song> songList;
     private Person[] person;
+    private SongReader songReader;
+    private int size;
     private int personListSize;
 
-    /**
-     * This method is the default constructor of the Survey sorter
-     * @param inputPerson
-     *              a person
-     * @param inputSongList
-     *              a SongList
-     * @param otherPersonListSize
-     *              the size of another person's list
-     * @throws FileNotFoundException
-     * @throws ParseException
-     */
-    public SurveySorter(Person[] inputPerson, SongList<Song> 
-        inputSongList, int otherPersonListSize) 
-        throws FileNotFoundException, ParseException
+    public SurveySorter(Person[] inputPerson, SongList<Song> inputSongList, int otherPersonListSize) throws FileNotFoundException, ParseException
     {
         person = inputPerson;
         songList = inputSongList;
         personListSize = otherPersonListSize;
+        size = 0;
     }
 
     /**
      * return the liked data of the hobby
-     * @return the data of sorting
-     * @param hobby the hobby input
-     * @param title the title of the song
-     * @param isHeardData if it is true
      */
-    public double dataOf(String title, Hobby hobby, Boolean isHeardData )
+    public int DataOf(String title, HobbyEnum hobby, Boolean isHeardData )
     {
         int count = 0;
         int i = 0;
-        while (i < personListSize)
+        while(i < personListSize)
         {
             if (person[i] == null)
             {
@@ -62,7 +43,7 @@ public class SurveySorter {
                 continue;
             }
 
-            Hobby thisHobby =  person[i].getHobby();
+            HobbyEnum thisHobby =  person[i].getHobby();
             if (thisHobby == hobby)
             {
                 SongList<Song> thisSongList = person[i].getSongList();
@@ -70,63 +51,42 @@ public class SurveySorter {
                 
                 if (isHeardData)
                 {
-                    if (thisSong != null && thisSong.getHeard() == 1)
-                    {
-                        count++;
-                    }
+                    if (thisSong != null && thisSong.getHeard() == 1) count++;
                 }
                 else 
                 {
-                    if (thisSong != null && thisSong.getLiked() == 1)
-                    {
-                        count++;
-                    }
+                    if (thisSong != null && thisSong.getLiked() == 1) count++;
                 }
             }
 
             i++;
         }
+        int output = 0;
+        if(this.numberOf(title, hobby, isHeardData)  != 0) {
+            output = count * 100 / this.numberOf(title, hobby, isHeardData) ;
 
-        double output = ((double)count / this
-            .numberOf(title, hobby, isHeardData) ) * 100;
+        }
+        //System.out.println( "Hobby: "+ hobby + " count: " + count +" number: " + this.numberOf(title, hobby, isHeardData) + " output: "  + output);
         return output;
     }
-    
-    /**
-     * This method returns the number of times within the data
-     * a person with a certain hobby heard the song
-     * @param title
-     *          the title of the song
-     * @param hobby
-     *          the hobby of the person
-     * @param isHeardData
-     *          boolean describing whether heard is true or false
-     * @return
-     *      the count
-     */
-    public double numberOf(String title, Hobby hobby, Boolean isHeardData)
+
+    public int numberOf(String title, HobbyEnum hobby, Boolean isHeardData)
     {
-        double count = 0.0000001;
+        int count = 0;
         for (int i = 0; i < person.length; i++)
         {
-            if (isHeardData)
+            if(isHeardData)
             {
-                if (person[i] != null && person[i].getHobby() 
-                    == hobby && person[i]
-                    .getSongList().getSongInLowerCase(title) != null
-                    && (person[i].getSongList()
-                    .getSongInLowerCase(title).getHeard() >= 0))
+                if (person[i] != null && person[i].getHobby()==hobby && person[i].getSongList().getSongInLowerCase(title) != null
+                && (person[i].getSongList().getSongInLowerCase(title).getHeard() >= 0))
                 {
                     count++;
                 }
             }
             else
             {
-                if (person[i] != null && person[i].getHobby() == 
-                    hobby && person[i].getSongList().getSongInLowerCase(title) 
-                    != null
-                    && (person[i].getSongList().getSongInLowerCase(title)
-                    .getLiked() >= 0))
+                if (person[i] != null && person[i].getHobby()==hobby && person[i].getSongList().getSongInLowerCase(title) != null
+                && (person[i].getSongList().getSongInLowerCase(title).getLiked() >= 0))
                 {
                     count++;
                 }
@@ -136,16 +96,23 @@ public class SurveySorter {
         
     }
 
+//    public Song getSong(String title, DLList<Song> curSongList)
+//    {
+//        Song targetSong = null;
+//        for (int k = 0; k < curSongList.size(); k++)
+//        {
+//            //System.out.println(curSongList.get(k).getSongTitle());
+//            if (curSongList.get(k).getSongTitle().equals(title))
+//            {
+//                //System.out.println(title);
+//                targetSong = curSongList.get(k);
+//            }
+//        }
+//
+// 
+//        return targetSong;
+//    }
 
-    /**
-     * This method will return the song in lower case
-     * @param title
-     *      the title of the song
-     * @param curSongList
-     *       the current songList
-     * @return
-     *      the targetSong
-     */
     public Song getSongInLowerCase(String title, DLList<Song> curSongList)
     {
         Song targetSong = null;
@@ -163,17 +130,7 @@ public class SurveySorter {
         return targetSong;
     }
     
-    /**
-     * The method will return the song's genre
-     * @param genre
-     *          the genre of the song
-     * @param curSongList
-     *          the current song list
-     * @return
-     *      the targetSong
-     */
-    public SongList<Song> 
-        getSongByGenre(String genre, SongList<Song> curSongList)
+    public SongList<Song> getSongByGenre(String genre, SongList<Song> curSongList)
     {
         SongList<Song> targetSong = new SongList<Song>();
         for (int k = 0; k < curSongList.size(); k++)
@@ -186,11 +143,7 @@ public class SurveySorter {
         return targetSong;
     }
 
-    /**
-     * This metho get the song list
-     * @return
-     *      the songList
-     */
+
     public SongList<Song> getSongList()
     {
         return songList;
