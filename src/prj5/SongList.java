4db0c858fe6e89cd7fe05b03150473f36204d564
ConnerMangin <cmangin@vt.edu>
2019-04-17 19:24:56 -0400

    Commented out getSong

--- src/prj5/SongList.java
+++ src/prj5/SongList.java
@@ -1,12 +1,12 @@
 /*
-* Virginia Tech Honor Code Pledge:
+ * Virginia Tech Honor Code Pledge:
  *
  * As a Hokie, I will conduct myself with honor
  * and integrity at all times.
  * I will not lie, cheat, or steal, nor will I
  * accept the actions of those who do.
  * -- Drew Pompeii (drewp24)
-
+ * 
  */
 
 package prj5;
@@ -14,10 +14,11 @@ package prj5;
 import java.util.Iterator;
 
 /**
- * This class manipulates the songs of a list by sorting, 
- * and keeping track of their stats
+ * This class manipulates the songs of a list by sorting,
+ * and keeping track of their stats.
  * 
- * @param <E>  the generic type for SongList
+ * @param <E>
+ *            the generic type for SongList
  *
  * @author Drew Pompeii (drewp24)
  * @version 04/14/2019
@@ -32,15 +33,16 @@ public class SongList<E> extends DLList<E> {
 
 
     /**
-     * This private method is used to sort DLLinked 
+     * This private method is used to sort DLLinked
      * list of strings alphabetically
-     * @param 
-     *      list the DLList that will be sorted
+     * 
+     * @param
+     * list
+     *            the DLList that will be sorted
      * @return
-     *      the alphabetized list
+     *         the alphabetized list
      */
-    private DLList<String> alphabeticalOrder(DLList<String> list) 
-    {
+    private DLList<String> alphabeticalOrder(DLList<String> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = 0; j < list.size() - 1; j++) {
                 if (list.get(j).compareTo(list.get(j + 1)) > 0) {
@@ -53,17 +55,16 @@ public class SongList<E> extends DLList<E> {
 
         }
         return list;
-    }
-    
-    
+    } 
+
 
     /**
      * This method will sort a list of artist names alphabetically
+     * 
      * @return
-     *      the alphabetized list of artist names
+     *         the alphabetized list of artist names
      */
-    public DLList<String> sortByArtistName() 
-    {
+    public DLList<String> sortByArtistName() {
         DLList<String> artistList = new DLList<String>();
         for (int x = 0; x < this.size(); x++) {
             artistList.add(((Song)this.get(x)).getArtistName());
@@ -71,13 +72,14 @@ public class SongList<E> extends DLList<E> {
         return alphabeticalOrder(artistList);
     }
 
+
     /**
      * This method will sort a list of song titles alphabetically
+     * 
      * @return
-     *      the alphabetized list of song titles
+     *         the alphabetized list of song titles
      */
-    public DLList<String> sortBySongTitle() 
-    {
+    public DLList<String> sortBySongTitle() {
         DLList<String> titleList = new DLList<String>();
         // System.out.println(this.size());
         for (int x = 0; x < this.size(); x++) {
@@ -89,19 +91,17 @@ public class SongList<E> extends DLList<E> {
     }
 
 
-
     /**
-     * This method will sort a list of integers representing 
-     * release years of songs 
+     * This method will sort a list of integers representing
+     * release years of songs
+     * 
      * @return
-     *      a list of release years sorted chronologically
+     *         a list of release years sorted chronologically
      */
-    public DLList<Integer> sortByReleaseYear() 
-    {
+    public DLList<Integer> sortByReleaseYear() {
         DLList<Integer> yearList = new DLList<Integer>();
         // System.out.println(this.size());
-        for (int x = 0; x < this.size(); x++) 
-        {
+        for (int x = 0; x < this.size(); x++) {
             yearList.add(((Song)this.get(x)).getDate());
         }
 
@@ -120,11 +120,13 @@ public class SongList<E> extends DLList<E> {
         return yearList;
     }
 
+
     /**
      * This method will sort a a list of genres alphabetically
+     * 
      * @return
-     *      return a list of genres sorted 
-     *      alphabetically(including capitalization)
+     *         return a list of genres sorted
+     *         alphabetically(including capitalization)
      */
     public DLList<String> sortByGenre() {
         DLList<String> genreList = new DLList<String>();
@@ -148,111 +150,123 @@ public class SongList<E> extends DLList<E> {
     }
 
 
-
     /**
-     * increment Heard 
-     * @param index  to keep track
+     * increment Heard
+     * 
+     * @param index
+     *            to keep track
      */
     public void heardToYes(int index) {
-        Song song = (Song) this.get(index);
+        Song song = (Song)this.get(index);
 
         this.remove(index);
         song.heardIsYes();
-        this.add(index, (E) song);
+        this.add(index, (E)song);
     }
 
-   
+
     /**
-     * increment Heard 
-     * @param index  to keep track
+     * increment Heard
+     * 
+     * @param index
+     *            to keep track
      */
     public void heardToNo(int index) {
-        Song song = (Song) this.get(index);
+        Song song = (Song)this.get(index);
 
-        
         this.remove(index);
         song.heardIsNo();
-        this.add(index, (E) song);
+        this.add(index, (E)song);
     }
+
+
     /**
-     * increment Heard 
-     * @param index  to keep track
+     * increment Heard
+     * 
+     * @param index
+     *            to keep track
      */
     public void likedToYes(int index) {
-        Song song = (Song) this.get(index);
+        Song song = (Song)this.get(index);
 
         this.remove(index);
         song.likedIsYes();
-        this.add(index, (E) song);
+        this.add(index, (E)song);
     }
+
+
     /**
-     * increment Heard 
-     * @param index  to keep track
+     * increment Heard
+     * 
+     * @param index
+     *            to keep track
      */
     public void likedToNo(int index) {
-        Song song = (Song) this.get(index);
+        Song song = (Song)this.get(index);
 
         this.remove(index);
         song.likedIsNo();
-        this.add(index, (E) song);
+        this.add(index, (E)song);
     }
 
-    /**
-     * Gets the song
-     * @param title  the title of the returning song
-     * @return the song searched for
-     */
-    public Song getSong(String title)
-    {
-        Song targetSong = null;
-        for (int k = 0; k < this.size(); k++)
-        {
-            //System.out.println(curSongList.get(k).getSongTitle());
-            if (((Song) this.get(k)).getSongTitle().equals(title))
-            {
-                //System.out.println(title);
-                targetSong = (Song) this.get(k);
-            }
-        }
 
-        return targetSong;
-    }
+//    /**
+//     * Gets the song
+//     * 
+//     * @param title
+//     *            the title of the returning song
+//     * @return the song searched for
+//     */
+//    public Song getSong(String title) {
+//        Song targetSong = null;
+//        for (int k = 0; k < this.size(); k++) {
+//            // System.out.println(curSongList.get(k).getSongTitle());
+//            if (((Song)this.get(k)).getSongTitle().equals(title)) {
+//                // System.out.println(title);
+//                targetSong = (Song)this.get(k);
+//            }
+//        }
+//
+//        return targetSong;
+//    }
+
 
     /**
      * Gets the song in lower case
-     * @param title  the title changing into lower case
+     * 
+     * @param title
+     *            the title changing into lower case
      * @return the song in lower case
      */
-    public Song getSongInLowerCase(String title)
-    {
+    public Song getSongInLowerCase(String title) {
         Song targetSong = null;
-        for (int k = 0; k < this.size(); k++)
-        {
-            //System.out.println(curSongList.get(k).getSongTitle());
-            if (((Song) this.get(k)).getSongTitle().toLowerCase().equals(title))
-            {
-                //System.out.println(title);
-                targetSong = (Song) this.get(k);
+        for (int k = 0; k < this.size(); k++) {
+            // System.out.println(curSongList.get(k).getSongTitle());
+            if (((Song)this.get(k)).getSongTitle().toLowerCase().equals(
+                title)) {
+                // System.out.println(title);
+                targetSong = (Song)this.get(k);
             }
         }
-
 
         return targetSong;
     }
 
+
     /**
      * Creates a string of the song list
+     * 
      * @return a string of the song list
      */
     public String toString() {
         String str = "";
 
         Iterator<E> list = this.iterator();
-        while (list.hasNext())
-        {
+        while (list.hasNext()) {
             str = str + list.next().toString() + "\n";
         }
-        //System.out.println(str);
+        // System.out.println(str);
 
         return str;
     }
+}
