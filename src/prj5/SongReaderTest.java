a337b8f453897c8573d09a15836c824f92e6ab29
drew_pompeii <drewp24@vt.edu>
2019-04-16 22:52:42 -0400

    song reader test javadoc

--- src/prj5/SongReaderTest.java
+++ src/prj5/SongReaderTest.java
@@ -3,7 +3,7 @@
 // As a Hokie, I will conduct myself with honor and integrity at all times.
 // I will not lie, cheat, or steal, nor will I accept the actions of those who
 // do.
-// -- cmangin
+// -- cmangin, drewp24
 
 package prj5;
 
@@ -14,7 +14,7 @@ import bsh.ParseException;
 /**
  * Tests the SongReader class.
  *
- * @author Conner Mangin (cmangin)
+ * @author Conner Mangin (cmangin) (drewp24)
  * @version 2019.04.14
  */
 public class SongReaderTest extends TestCase {
@@ -33,6 +33,7 @@ public class SongReaderTest extends TestCase {
     }
 
     /**
+     * Tests the readSongFile method
      * @throws java.text.ParseException 
      * @throws FileNotFoundException 
      * 
