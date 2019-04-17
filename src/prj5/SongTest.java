ae8be14f9d43471ebf0badb48cc971a975a7166b
ConnerMangin <cmangin@vt.edu>
2019-04-16 21:54:51 -0400

    song test change

--- src/prj5/SongTest.java
+++ src/prj5/SongTest.java
@@ -1,14 +1,8 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- cmangin
-
 package prj5;
+
 import student.TestCase;
 
-/*   
+/*
  * Virginia Tech Honor Code Pledge:
  *
  * As a Hokie, I will conduct myself with honor
@@ -16,7 +10,7 @@ import student.TestCase;
  * I will not lie, cheat, or steal, nor will I
  * accept the actions of those who do.
  * -- Drew Pompeii (drewp24)
-
+ * 
  */
 
 /**
@@ -25,79 +19,93 @@ import student.TestCase;
  * @author Drew Pompeii (drewp24)
  * @version 2019.04.16
  */
-public class SongTest extends TestCase
-{
+public class SongTest extends TestCase {
     private Song song;
-    
+
+
     /**
      * Sets up before each test class is ran
      */
-    public void setUp()
-    {
-        song = new Song("The Killers", "All These Things I've Done", 
-            "alternative", 2005);
+    public void setUp() {
+        song = new Song("The Killers", "All These Things I've Done",
+            "Alternative", 2005);
     }
-    
+
+
     /**
      * Test the getArtistName method
      */
-    public void testGetAristName()
-    {
+    public void testGetAristName() {
         assertEquals(song.getArtistName(), "The Killers");
     }
-    
+
+
     /**
      * Test the getSongTitle method
      */
-    public void testGetSongTitle()
-    {
+    public void testGetSongTitle() {
         assertEquals(song.getSongTitle(), "All These Things I've Done");
     }
-    
+
+
     /**
      * Test the getGenre method
      */
-    public void testGetGenre()
-    {
-        assertEquals(song.getGenre(), "alternative");
+    public void testGetGenre() {
+        assertEquals(song.getGenre(), "Alternative");
     }
-    
+
+
     /**
      * Test the getDate method
      */
-    public void testGetDate()
-    {
+    public void testGetDate() {
         assertEquals(song.getDate(), 2005);
     }
-    
+
+
     /**
      * Test the getLiked and the addLiked method
      */
-    public void testAddLiked()
-    {
-        assertEquals(song.getLiked(), 50);
-        //song.addLiked();
-        assertEquals(song.getLiked(), 51);
+    public void testHeardAndLikeIsNo() {
+        assertEquals(song.getLiked(), -1);
+        song.likedIsNo();
+        assertEquals(song.getLiked(), 0);
+        assertEquals(song.getHeard(), -1);
+        song.heardIsNo();
+        assertEquals(song.getHeard(), 0);
+
     }
-    
+
+
     /**
      * Test the getHeard and addHeard method
      */
-    public void testAddHeard()
-    {
-        assertEquals(song.getHeard(), 100);
-        //song.addHeard();
-        assertEquals(song.getHeard(), 101);
+    public void testHeardAndLikeIsYes() {
+        assertEquals(song.getLiked(), -1);
+        song.likedIsYes();
+        assertEquals(song.getLiked(), 1);
+        assertEquals(song.getHeard(), -1);
+        song.heardIsYes();
+        assertEquals(song.getHeard(), 1);
     }
+
 
     /**
      * Test the toString method
      */
-    public void testToString()
-    {
+    public void testToString() {
         assertEquals(song.toString(), "Title: All These Things I've Done; "
             + "Artist: The Killers; Genre: Alternative; Date: 2005;"
-            + " Liked: 50; Heard: 100");
+            + " Liked: -1; Heard: -1");
+    }
+    /**
+     * This tests the testToStringTest method.
+     */
+    public void testToStringTest()
+    {
+        assertEquals(song.toStringTest(), "song title all these things i've"
+            + " done" + "\n" + "song artist the killers" + "\n" + "song "
+                + "genre Alternative" + "\n" +  "song year 2005");
     }
 }
-
