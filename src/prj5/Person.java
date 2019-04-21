046b2eba60e28575bc5f22040a611970d01da9c0
ConnerMangin <cmangin@vt.edu>
2019-04-21 13:51:27 -0400

    enum change

--- src/prj5/Person.java
+++ src/prj5/Person.java
@@ -17,57 +17,65 @@ package prj5;
 public class Person {
 
     private SongList<Song> songList;
-    private Hobby hobby;
-    private Major major;
-    private State state;
+    private HobbyEnum hobby;
+    private MajorEnum major;
+    private StateEnum state;
     private int index;
-    
+
+
     /**
      * This is the constructor for the Person class
-     * @param thisSongList songList passed through
-     * @param thisHobby the person's hobby
-     * @param thisState the person's state
-     * @param thisMajor the person's major
-     * @param thisIndex the person's index
+     * 
+     * @param thisSongList
+     *            songList passed through
+     * @param thisHobby
+     *            the person's hobby
+     * @param thisState
+     *            the person's state
+     * @param thisMajor
+     *            the person's major
+     * @param thisIndex
+     *            the person's index
      */
-    public Person(SongList<Song> thisSongList, 
-        String thisHobby, String thisState, String thisMajor, int thisIndex)
-    {
+    public Person(
+        SongList<Song> thisSongList,
+        String thisHobby,
+        String thisState,
+        String thisMajor,
+        int thisIndex) {
 
         songList = thisSongList;
         // determines the hobby
-        switch (thisHobby) 
-        {
+        switch (thisHobby) {
             case "reading":
-                hobby = Hobby.READ;
+                hobby = HobbyEnum.READ;
                 break;
             case "art":
-                hobby = Hobby.ART;
+                hobby = HobbyEnum.ART;
                 break;
             case "music":
-                hobby = Hobby.MUSIC;
+                hobby = HobbyEnum.MUSIC;
                 break;
             case "sports":
-                hobby = Hobby.SPORTS;
+                hobby = HobbyEnum.SPORTS;
                 break;
             default:
                 hobby = null;
                 break;
         }
         // determines the major
-        switch (thisMajor) 
-        {
+        switch (thisMajor) {
             case "Computer Science":
-                major = Major.CS;
+                major = MajorEnum.CS;
                 break;
             case "Math or CMDA":
-                major = Major.MATH_OR_CMDA;
+                major = MajorEnum.MATH_OR_CMDA;
                 break;
             case "Other":
-                major = Major.OTHER;
+                major = MajorEnum.OTHER;
                 break;
             case "Other Engineering":
-                major = Major.OTHER_ENGE;
+                major = MajorEnum.OTHER_ENGE;
                 break;
             default:
                 major = null;
@@ -75,19 +83,18 @@ public class Person {
         }
 
         // determines the state
-        switch (thisState) 
-        {
+        switch (thisState) {
             case "Northeast":
-                state = State.NORTHEAST_US;
+                state = StateEnum.NORTHEAST_US;
                 break;
             case "Outside of United States":
-                state = State.OUTSIDE_THE_US;
+                state = StateEnum.OUTSIDE_THE_US;
                 break;
             case "Southeast":
-                state = State.SOUTHEAST_US;
+                state = StateEnum.SOUTHEAST_US;
                 break;
             case "United States (other than Southeast or Northwest)":
-                state = State.THE_REST_OF_US;
+                state = StateEnum.THE_REST_OF_US;
                 break;
             default:
                 state = null;
@@ -97,61 +104,65 @@ public class Person {
         index = thisIndex;
     }
 
+
     /**
      * get the hobby data of the person
+     * 
      * @return the hobby data
      */
-    public Hobby getHobby()
-    {
+    public HobbyEnum getHobby() {
         return hobby;
     }
 
+
     /**
      * get the major data of the person
+     * 
      * @return the major data
      */
-    public Major getMajor()
-    {
+    public MajorEnum getMajor() {
         return major;
     }
 
+
     /**
      * get the state data of the person
+     * 
      * @return the state data
      */
-    public State getState()
-    {
+    public StateEnum getState() {
         return state;
     }
 
+
     /**
      * get the index of the person
+     * 
      * @return the index of the person
      */
-    public int getIndex()
-    {
+    public int getIndex() {
         return index;
     }
 
+
     /**
      * gets the song list of the person.
+     * 
      * @return the song list data
      */
-    public SongList<Song> getSongList()
-    {
+    public SongList<Song> getSongList() {
         return songList;
     }
 
+
     /**
      * return the content of this person
+     * 
      * @return the string content of this person
      */
-    public String toString()
-    {
-        return index + "; Hobby: " 
-            + hobby + "; Major: "  + major
-            + "; State: " + state;
+    public String toString() {
+        return index + "; Hobby: " + hobby + "; Major: " + major + "; State: "
+            + state;
     }
-
 
 }
